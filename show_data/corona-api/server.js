fb89a378f915a2abbcfb48035993656b6c03dab8
Nicolas Voigt <1752371+nicolas-voigt@users.noreply.github.com>
2020-03-22 00:01:14 +0100

    WIP API ver 0.1

---
+++ show_data/corona-api/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/get_articles_by_city", (req, res) => {
+});
+app.post("/get_articles_by_state", (req, res) => {
+
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
