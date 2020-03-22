1a05112107687ca573189e9a42ea56fd1de326b2
Nicolas Voigt <1752371+nicolas-voigt@users.noreply.github.com>
2020-03-22 12:07:20 +0100

    server mock-up

---
+++ show_data/corona-express/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const secure = require('express-force-https');
+
+// Credentials for HTTPS
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/infocovid19.de/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/infocovid19.de/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/infocovid19.de/chain.pem', 'utf8');
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
+
+let app = express();
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+app.use(secure);
+app.use(express.static('../corona-app/build/'));
