760ac2453b40a117666d2051b3862a4781bc8aa5
Nicolas Voigt <1752371+nicolas-voigt@users.noreply.github.com>
2020-03-22 12:25:30 +0100

    HTTP to HTTPS redirector update

--- show_data/corona-express/server.js
+++ show_data/corona-express/server.js
@@ -16,12 +16,16 @@ const credentials = {
 
 
 let app = express();
-const httpServer = http.createServer(app);
+const httpServer = express.createServer();
+
+httpServer.get('*', function(req, res) {
+    res.redirect('https://' + req.headers.host + req.url);
+})
+
 const httpsServer = https.createServer(credentials, app);
-app.use(secure);
 app.use('/', express.static('../corona-app/build/'));
 httpServer.listen(80, () => {
-	console.log('HTTP Server running on port 80');
+  console.log('HTTP redirector running on port 80')
 });
 
 httpsServer.listen(443, () => {
