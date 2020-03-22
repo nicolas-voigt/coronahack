6303c660126dde9f1ac5467058d20bd41c74be99
Nicolas Voigt <1752371+nicolas-voigt@users.noreply.github.com>
2020-03-22 14:33:21 +0100

    update fetch to axios

--- show_data/corona-app/src/services/RestService.js
+++ show_data/corona-app/src/services/RestService.js
@@ -1,4 +1,6 @@
 import APIKeys from '../APIKeys.ignore.js';
+import axios from 'axios';
+
 const geocodeURL = "https://maps.googleapis.com/maps/api/geocode/json?result_type=administrative_area_level_1|country|locality&latlng="
 
 class RestService {
@@ -7,27 +9,13 @@ class RestService {
   }
 
   async retrieveArticlesForState(state) {
-    let response = await fetch(this.url, {
-      method: 'POST',
-      body: JSON.stringify({state})
-    });
-    if (response.ok) {
-      let jsonResult = await response.json();
-      return jsonResult.city;
-    }
-    throw new Error('API Error occured');
+    let response = await axios.post(this.url,{state});
+    return response.city;
   }
 
   async retrieveArticlesForCity(city) {
-    let response = await fetch(this.url, {
-      method: 'POST',
-      body: JSON.stringify({city})
-    });
-    if (response.ok) {
-      let jsonResult = await response.json();
-      return jsonResult.state;
-    }
-    throw new Error('API Error occured');
+    let response = await axios.post(this.url, {city});
+    return response.state;
   }
 
   async retrieveArticlesFromLocation(lat, lng) {
@@ -36,29 +24,23 @@ class RestService {
     let state = undefined;
     let city = undefined;
 
-    let response = await fetch(url);
-    if (response.ok) {
-      let json = await response.json();
-      let firstResult = json.results[0].address_components;
-      for (let resultData of firstResult) {
-        if (resultData.types.includes("locality")) {
-          city = resultData.long_name;
-        }
-        if (resultData.types.includes("administrative_area_level_1")) {
-          state = state.long_name;
-        }
+    let response = await axios.get(url);
+    let firstResult = response.results[0].address_components;
+    for (let resultData of firstResult) {
+      if (resultData.types.includes("locality")) {
+        city = resultData.long_name;
       }
-      if (city !== undefined) {
-        retArr.concat(await this.retrieveArticlesForCity(city));
+      if (resultData.types.includes("administrative_area_level_1")) {
+        state = state.long_name;
       }
-      if (state !== undefined) {
-        retArr.concat(await this.retrieveArticlesForState(state));
-      }
-      return retArr;
-
-    } else {
-      throw new Error('Geolocation failed');
     }
+    if (city !== undefined) {
+      retArr.concat(await this.retrieveArticlesForCity(city));
+    }
+    if (state !== undefined) {
+      retArr.concat(await this.retrieveArticlesForState(state));
+    }
+    return retArr;
   }
 }
 
