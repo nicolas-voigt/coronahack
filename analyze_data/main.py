01abf55224df7dcff7215c6f6c4b3f55db4a2a36
McToel <theo.doellmann@gmx.de>
2020-03-22 17:13:23 +0100

    changed port

--- analyze_data/main.py
+++ analyze_data/main.py
@@ -9,10 +9,15 @@ import json
 import sqlalchemy
 import datetime
 
-#We are using bottle for the api
-from bottle import request, response, run
-from bottle import post, get, put, delete
-import bottle
+#f u bottle I'm now using a Flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+#allow cors
+CORS(app)
+
 import config
 
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://' + config.username + ':'+ config.password + '@' + config.server+ '/' + config.database
@@ -21,32 +26,6 @@ SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://' + config.username + ':'+ config.pas
 engine = sqlalchemy.create_engine(SQLALCHEMY_DATABASE_URI)
 # print(engine.table_names())
 
-def add_cors_headers():
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = \
-        'GET, POST, PUT, OPTIONS'
-    response.headers['Access-Control-Allow-Headers'] = \
-        'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
-
-@bottle.route('/<:re:.*>', method='OPTIONS')
-def enable_cors_generic_route():
-    """
-    This route takes priority over all others. So any request with an OPTIONS
-    method will be handled by this function.
-
-    See: https://github.com/bottlepy/bottle/issues/402
-
-    NOTE: This means we won't 404 any invalid path that is an OPTIONS request.
-    """
-    add_cors_headers()
-
-@bottle.hook('after_request')
-def enable_cors_after_request_hook():
-    """
-    This executes after every route. We use it to attach CORS headers when
-    applicable.
-    """
-    add_cors_headers()
 
 def get_source_score(source_url):
     super_trust_sources = []
@@ -74,7 +53,7 @@ def calc_trust_score(data):
 def upload(data, table):
     data.to_sql('table', con=engine, if_exists='append', method='multi')
 
-@post('/rate') #https://stackoverflow.com/questions/34661318/replace-rows-in-mysql-database-table-with-pandas-dataframe
+@app.route('/rate', methods=['POST']) #https://stackoverflow.com/questions/34661318/replace-rows-in-mysql-database-table-with-pandas-dataframe
 def rate_data():
     try:
         # parse input data
@@ -91,7 +70,7 @@ def rate_data():
         except:
             raise ValueError
     except ValueError:
-        response.status = 400
+        #response.status = 400
         return
 
     news_data['total_ratings'] += 1
@@ -102,9 +81,9 @@ def rate_data():
     cursor.execute(delete_str)
     engine.commit()
     news_data.to_sql('news', if_exists='append', con=engine)
+    #add_cors_headers()
 
-
-@post('/get_data')
+@app.route('/get_data', methods=['POST'])
 def creation_handler():
     request_data = {}
     data = {'country':pd.DataFrame(), 'state':pd.DataFrame(), 'city':pd.DataFrame()}
@@ -144,11 +123,11 @@ def creation_handler():
         
     except ValueError:
         # if bad request data, return 400 Bad Request
-        response.status = 400
+        #response.status = 400
         return
     except KeyError:
         # if bad request data, return 400 Bad Request
-        response.status = 400
+        #response.status = 400
         return
     
     # TODO: Add trust score
@@ -165,10 +144,13 @@ def creation_handler():
         else:
             data[key] = ''
 
-    response.headers['Content-Type'] = 'application/json'
+    #response.headers['Content-Type'] = 'application/json'
+    #add_cors_headers()
     return json.dumps({"country":data['country'], "state":data['state'], "city":data['city']}, ensure_ascii=False)
 
+# context.use_certificate_file('yourserver.crt')
 
 if __name__ == '__main__':
-    run(host = '127.0.0.1', port = 8000)
+    context = ('/etc/letsencrypt/live/infocovid19.de/cert.pem', '/etc/letsencrypt/live/infocovid19.de/privkey.pem')#certificate and key files
+    app.run(host= '127.0.0.1', port=8000, ssl_context=context)
 
