9c82b0301c44b626fff5136b3d354b2deb993ac9
Nicolas Voigt <1752371+nicolas-voigt@users.noreply.github.com>
2020-03-22 14:08:21 +0100

    updated index.js

--- show_data/corona-app/src/components/map/CoronaMarkers/index.js
+++ show_data/corona-app/src/components/map/CoronaMarkers/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import {CoronaMarker} from "./CoronaMarker";
-//import {Markers} from "./MarkersData";
+import {Markers} from "./MarkersData";
 import RestService from '../../../services/RestService.js';
 
 let api = new RestService();
-
-let findFromHerne = () => {
+let CoronaMarkers = () => {
   api.retrieveArticlesFromCity('Herne').then((results) => {
     results.map((marker, index) => (
       <CoronaMarker
@@ -13,10 +12,11 @@ let findFromHerne = () => {
           lat={52.5200}
           lng={13.4050}
           factor={marker.factor}
-          link={marker.url}
+          link={marker.link}
           text={marker.text}
       />
     ))
-  });
+});
 }
-export default findFromHerne
+
+export default CoronaMarkers;
